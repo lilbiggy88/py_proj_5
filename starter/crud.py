921e74b5e1243d4be53fd17018e2b20a8597ba66
John Biggerstaff <john.r.biggerstaff@gmail.com>
2023-05-08 22:41:26 -0600

    almost done with part 3

--- starter/crud.py
+++ starter/crud.py
@@ -17,12 +17,22 @@ def create_movie(title, overview, release_date, poster_path):
 
     return movie
 
+def get_movies():
+    """This will return all movies"""
+    
+    return Movie.query.all()
+
 def create_rating(user, movie, score):
     """Create and return a new rating."""
 
     rating = Rating(user=user, movie=movie, score=score)
 
     return rating
+
+def get_movie_by_id(movie_id):
+    
+    return Movie.query.get()
+
 
 if __name__ == '__main__':
     from server import app
